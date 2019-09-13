d1edf549a5b8307376a1af9b99643ee9e8042d09
Walker <walker233@users.noreply.github.com>
2019-09-13 12:37:49 -0800

    Update package_spec.rb

--- controls/package_spec.rb
+++ controls/package_spec.rb
@@ -89,7 +89,6 @@ control 'package-08' do
   impact 1.0
   title 'Install auditd'
   desc 'auditd provides extended logging capabilities on recent distributions'
-  only_if { !container_execution }
   audit_pkg = os.redhat? || os.suse? || os.name == 'amazon' || os.name == 'fedora' ? 'audit' : 'auditd'
   describe package(audit_pkg) do
     it { should be_installed }
